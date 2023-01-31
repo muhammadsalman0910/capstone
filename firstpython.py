5d008c8fa59f1c75df3ecb5969a6d671831567ac
muhammadsalman0910 <123708408+muhammadsalman0910@users.noreply.github.com>
2023-01-31 16:08:50 -0500

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+# ^^^ pyforest auto-imports - don't write above this line
+#install packages
+!pip install geopy
+!pip install lxml
+!pip install folium
+print('Installed.')
